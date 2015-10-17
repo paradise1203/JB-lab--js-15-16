a33db6a514858817f0b0a43d173b74cc08ea524e
Shayfutdinov Aidar <exoooy@gmail.com>
2015-10-17 15:12:38 +0300

    Class work

---
+++ classworks/cw_05/script.js
@@ -0,0 +1,22 @@
+$(function () {
+    var temp;
+    temp = doT.template(
+        $('#temp1').text()
+    );
+    /*
+     console.log(temp('Hello World!'));
+     $('body').html(temp([1, 2, 3, 4, 5, 6]));
+     */
+
+    $("#send").click(function () {
+        var color = $("select").val();
+        var message = $("<div>").append(
+            temp({
+                text: $("#message").val(),
+                date: new Date()
+            })
+        );
+        message.css("background-color", color);
+        $("#dialog").append(message);
+    });
+});
